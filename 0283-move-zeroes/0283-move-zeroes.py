543ea91db9a8df613faa569e126d7df45a1d5dcd
Liz Abraham <148650500+liz-ab@users.noreply.github.com>
2026-07-04 12:19:34 +0530

    Time: 2 ms (92.91%), Space: 13.7 MB (19.13%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        i,j=0,0
+        for i in range(len(nums)):
+            if(nums[i]!=0):
+                nums[i],nums[j]=nums[j],nums[i]
+                j+=1
+        return nums
+
+        
