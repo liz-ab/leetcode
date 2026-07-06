1440771ebc29cb42b7276ef7d5abc2925f42b1c1
Liz Abraham <148650500+liz-ab@users.noreply.github.com>
2026-07-06 10:12:04 +0530

    Time: 56 ms (77.31%), Space: 21.9 MB (20.2%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -2,8 +2,6 @@ class Solution(object):
     def maxSubArray(self, nums):
         if(len(nums)==1):
             return nums[0]
-        if(len(nums)==2):
-            return max(sum(nums),max(nums))
         cur=0
         ma=0
         for i in nums:
