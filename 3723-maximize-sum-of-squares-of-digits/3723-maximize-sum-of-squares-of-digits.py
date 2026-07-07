98e16f32185749115244fb66c0d06a785810a94b
Liz Abraham <148650500+liz-ab@users.noreply.github.com>
2026-07-07 09:39:51 +0530

    Time: 1856 ms (53.33%), Space: 21.1 MB (33.33%) - LeetHub

---
+++ 3723-maximize-sum-of-squares-of-digits/3723-maximize-sum-of-squares-of-digits.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def maxSumOfSquares(self, num, sum):
+        if sum>9*num:
+            return ""
+        res=""
+        for i in range(num):
+            digit=min(9,sum)
+            res=res+str(digit)
+            sum-=digit
+        if sum!=0:
+            return ""
+        return res
+
+        
