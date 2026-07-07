089a07d417b9fc98cd81e28176e9ba963b1f43da
Liz Abraham <148650500+liz-ab@users.noreply.github.com>
2026-07-07 11:29:34 +0530

    Time: 0 ms (100%), Space: 12.3 MB (89.21%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def mySqrt(self, x):
+        if(x<2):
+            return x
+        low,high=1,x//2
+        while(low<=high):
+            mid=low+(high-low)//2
+            if mid*mid>x:
+                high=mid-1
+            else:
+                low=mid+1
+        return low-1
+        
+        
