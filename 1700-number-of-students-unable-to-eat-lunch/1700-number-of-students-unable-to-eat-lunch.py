e986a8bd55f9d18bbb2c23832d5817a7522417e2
Liz Abraham <148650500+liz-ab@users.noreply.github.com>
2026-07-08 11:25:38 +0530

    Time: 0 ms (100%), Space: 12.5 MB (20.94%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
@@ -1,18 +1,10 @@
 class Solution(object):
     def countStudents(self, st, sa):
-        stack=[]
-        c=0
-        length=(len(st)*len(sa))//2
-        while(c!=length):
-            if(len(st)==0):
-                return 0
-            if(st[0]==sa[0]):
-                st.pop(0)
-                sa.pop(0)
-            else:
-                c=c+1
-                k=st.pop(0)
-                st.append(k)
-        return len(st)
+        count=[st.count(0),st.count(1)]
+        for i in sa:
+            if(count[i]==0):
+                break
+            count[i]-=1
+        return sum(count)
 
         
