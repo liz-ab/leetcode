e7d5e1a340626370c8744b6eb19447cf08eb4bc8
Liz Abraham <148650500+liz-ab@users.noreply.github.com>
2026-07-09 10:48:36 +0530

    Time: 175 ms (40.38%), Space: 14.9 MB (96.15%) - LeetHub

---
+++ 3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def minOperations(self, nums):
+        l=0
+        k=2
+        c=0
+        while(k<len(nums)):
+            if(nums[l]==0):
+                nums[l]=None
+                l=l+1
+                nums[l]=abs(1-nums[l])
+                nums[k]=abs(1-nums[k])
+                k=k+1
+                c+=1
+            else:
+                nums[l]=None
+                l+=1
+                k+=1
+        while(l<len(nums)):
+            if(nums[l]==1):
+                nums[l]=None
+                l+=1
+            else:
+                return -1
+        return c
+            
+
+
