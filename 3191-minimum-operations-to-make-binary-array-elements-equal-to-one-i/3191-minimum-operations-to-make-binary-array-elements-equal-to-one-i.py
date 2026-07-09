72d33d8d3a6298e011470b4bec8915b227670025
Liz Abraham <148650500+liz-ab@users.noreply.github.com>
2026-07-09 11:59:13 +0530

    Time: 157 ms (57.69%), Space: 15.1 MB (96.15%) - LeetHub

--- 3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i.py
+++ 3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3191-minimum-operations-to-make-binary-array-elements-equal-to-one-i.py
@@ -5,14 +5,12 @@ class Solution(object):
         c=0
         while(k<len(nums)):
             if(nums[l]==0):
-                nums[l]=None
                 l=l+1
                 nums[l]=abs(1-nums[l])
                 nums[k]=abs(1-nums[k])
                 k=k+1
                 c+=1
             else:
-                nums[l]=None
                 l+=1
                 k+=1
         while(l<len(nums)):
