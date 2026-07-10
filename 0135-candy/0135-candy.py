3a1d5944992b0697f0c49d522e79d9c45e9b7b16
Liz Abraham <148650500+liz-ab@users.noreply.github.com>
2026-07-10 10:38:49 +0530

    Time: 26 ms (17.35%), Space: 14.6 MB (83.12%) - LeetHub

---
+++ 0135-candy/0135-candy.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def candy(self, ratings):
+        candies=[1]*len(ratings)
+        for i in range(1,len(ratings)):
+            if(ratings[i]>ratings[i-1]):
+                candies[i]=candies[i-1]+1
+        for i in range(len(ratings)-2,-1,-1):
+            if(ratings[i]>ratings[i+1]):
+                candies[i]=max(candies[i],candies[i+1]+1)
+        return sum(candies)
+        
