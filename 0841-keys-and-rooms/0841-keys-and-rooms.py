94a564b707645bc1973c5e69af0a9e06895c3f4a
Liz Abraham <148650500+liz-ab@users.noreply.github.com>
2026-07-13 11:34:23 +0530

    Time: 1 ms (51.19%), Space: 12.6 MB (86.04%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.py
+++ 0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -1,16 +1,12 @@
 class Solution(object):
     def canVisitAllRooms(self, rooms):
-        graph=[[] for _ in range(len(rooms))]
-        for i in range(len(rooms)):
-            for num in rooms[i]:
-                graph[i].append(num)
         stack=[0]
         visited=[False]*len(rooms)
         while stack:
             cur=stack.pop()
             if not visited[cur]:
                 visited[cur]=True
-                for neigh in graph[cur]:
+                for neigh in rooms[cur]:
                     if not visited[neigh]:
                         stack.append(neigh)
         if False in visited:
