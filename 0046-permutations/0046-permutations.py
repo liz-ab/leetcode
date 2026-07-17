9392c5cd586a31c2c8f0631805cb53d46c3f11d6
Liz Abraham <148650500+liz-ab@users.noreply.github.com>
2026-07-17 09:51:09 +0530

    Time: 0 ms (100%), Space: 19.6 MB (9.49%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,18 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res=[]
+        check={num:False for num in nums}
+        def backtrack(current,check):
+            if(len(current)==len(nums)):
+                res.append(current.copy())
+                return
+            for i in nums:
+                if check[i]:
+                    continue 
+                check[i]=True
+                current.append(i)
+                backtrack(current,check)
+                current.pop()
+                check[i]=False
+        backtrack([],check)
+        return res
