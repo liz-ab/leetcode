7b7796f37b82f695fba938775b869b68f5fa1fe5
Liz Abraham <148650500+liz-ab@users.noreply.github.com>
2026-07-17 11:25:36 +0530

    Time: 0 ms (100%), Space: 19 MB (97.64%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.py
+++ 0231-power-of-two/0231-power-of-two.py
@@ -1,5 +1,3 @@
-class Solution(object):
-    def isPowerOfTwo(self, n):
-       return (n>0 and ((n&n-1)==0))
-
-        
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        return n>0 and n&n-1==0
