e52787c05eef9fdffc6ead3cd003bec55eed5634
Liz Abraham <148650500+liz-ab@users.noreply.github.com>
2026-07-18 08:51:23 +0530

    Time: 201 ms (33.72%), Space: 34.7 MB (84.8%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -9,7 +9,7 @@ class Solution:
             q.append(r)
             if l>q[0]:
                 q.popleft()
-            if (r+1)>=k:
+            if r+1>=k:
                 output.append(nums[q[0]])
                 l+=1
             r+=1
