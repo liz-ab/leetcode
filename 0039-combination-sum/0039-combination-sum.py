b4ebc7683443aa5804a8df8b07dd39259063e1ab
Liz Abraham <148650500+liz-ab@users.noreply.github.com>
2026-09-26 17:24:53 +0530

    Time: 27 ms (14.5%), Space: 12.6 MB (19.79%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def combinationSum(self, candidates, target):
+        res=[]
+        def dfs(i,cur,total):
+            if(total==target):
+                t=cur[:]
+                res.append(t)
+                return
+            if(i>=len(candidates) or total>target):
+                return
+            cur.append(candidates[i])
+            dfs(i,cur,total+candidates[i])
+            cur.pop()
+            dfs(i+1,cur,total)
+        dfs(0,[],0)
+        return res
+        
